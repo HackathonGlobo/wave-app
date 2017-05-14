35fca28ef88ad526979dbc4fd266c49137d0a30d
akaFTS <gustavo84171@hotmail.com>
2017-05-14 03:07:15 -0300

    testando segunda onda 3

--- GloboWave/www/js/controllers.js
+++ GloboWave/www/js/controllers.js
@@ -59,8 +59,8 @@ angular.module('WaveApp')
         
         function analyseCycle() {
             analyser.getByteFrequencyData(dataArray);
-            console.log(dataArray[915]);
-            console.log(dataArray[925]);
+            fineArray = dataArray.slice(815, 825);
+            console.log(fineArray);
             // if(dataArray[896] > 120) {
             //     console.log("Peguei a frequencia porra");
             //    $timeout(analyseCycle, 120000);   
